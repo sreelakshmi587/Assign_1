dc1338ae27c2563032de1132f2870408874c64e8
sreelakshmi587 <90371930+sreelakshmi587@users.noreply.github.com>
2021-09-19 22:51:58 +0530

    Update Assign_2.py

--- Assign_2.py
+++ Assign_2.py
@@ -1,120 +1,95 @@
+print(f"Welcome to the game of Tic-Tac-Toe...Be ready to roll on...")
+game_board={7:' ',8:' ',9:' ',4:' ',5:' ',6:' ',1:' ',2:' ',3:' '}
+symbol_chosen={}
+Winner={}
+board_history=[]
+i=1;j=1
+storing=[]
 
 def display_board(board):
-    print('\n'*10)
-
-    print("\t     |     |")
-    print("\t  {}  |  {}  |  {}".format(board[7], board[8], board[9]))
-    print('\t_____|_____|_____')
-
-    print("\t     |     |")
-    print("\t  {}  |  {}  |  {}".format(board[4], board[5], board[6]))
-    print('\t_____|_____|_____')
-
-    print("\t     |     |")
-
-    print("\t  {}  |  {}  |  {}".format(board[1], board[2], board[3]))
-    print("\t     |     |")
-    print("\n")
-
+  for i in board:
+      print('|', board[i], '|', end="")
+      if i % 3 == 0:
+          print()
 
 def player_input():
     symbol = ''
-    while symbol!='X' and symbol!='O':
-        symbol= input("User:Please Choose either X or O: ").upper()
+    while symbol != 'X' and symbol != 'O':
+        symbol = input("User:Please Choose either X or O: ").upper()
     if symbol == "X":
         return ('X','O')
     else:
-        return ('O',X)
+        return ('O','X')
+User_choice,Computer_choice=player_input()
+print(f'User chose {User_choice}''\n',f'Computer chose {Computer_choice}')
 
-
-
-User,Computer = player_input()
-
-def place_symbol(board,symbol,position):
-    board[position] = symbol
-
-def win_check(board,symbol):
-
-    return ((board[1]==symbol and board[2]==symbol and board[3]==symbol) or
-            (board[4] == symbol and  board[5] ==symbol and  board[6] == symbol) or
-            (board[7] == symbol and  board[8] == symbol and board[9] == symbol) or
-            (board[7] == symbol and  board[4] == symbol and  board[1] == symbol) or
-            (board[8]== symbol and board[5]==symbol and board[1]==symbol) or
-            (board[9] == symbol and board[6] == symbol and  board[3] == symbol) or
-            (board[7]== symbol and board[5]==symbol and board[3]==symbol) or
-            (board[9] == symbol and  board[5] ==symbol and  board[1] == symbol))
-import random
-def who_first():
-    flip= random.randint(0,1)
-    if flip ==0:
-        return 'User'
-    else:
-        return 'Computer'
-
-def game_space_check(board,position):
-    return board[position]==' '
 def full_board_check(board):
-    for i in range(1,10):
-        if game_space_check(board,i):
+    for position in range(1, 10):
+        if board[position]==' ':
             return False
     return True
+
+computer_position= 0; user_position=0
 def player_choice(board):
-    position = 0
-    while position not in range(1,10) or not game_space_check(board,position):
-        position= int(input('Choose a position[0-9]: '))
-    return position
-def replay():
-    for game in range(1,11):
-        input("Wanna play again?Enter Y or N")
-        return choice == 'Y'
-
-print("Welcome!! ...Let's roll on to the game-Tic-Tac-Toe")
-while True:
-    game_board = [' '] * 10
-    User_symbol, Computer_symbol = player_input()
-    turn = who_first()
-    print(turn + ' will go first')
-    game_initiater = input('Ready to play?Y or N: ').upper()
-    if game_initiater == 'Y':
-        begin_game = True
-    else:
-        begin_game = False
-
-    while begin_game:
-        game_winner = []
-        if turn == 'User':
-            print(display_board(game_board))
-            position = player_choice(game_board)
-            place_symbol(game_board, User_symbol, position)
-            if win_check(game_board, User_symbol):
-                print(display_board(game_board))
-                game_winner.append(['User', User_symbol])
-                print(f'User wins game {i}')
-                begin_game = False
-            else:
-
-                if full_board_check(game_board):
-                    print(display_board(game_board))
-                    print("It's a Tie")
-                    game_winner.append('Tie')
-                    begin_game = False
-                else:
-                    turn = 'Computer'
+    user_position = int(input('Choose a position[0-9]: '))
+    if user_position <= 9:
+        if board[user_position] == ' ':
+            board[user_position] = User_choice
         else:
-            print(display_board(game_board))
-            possible_choices = list(range(1, 10))
-            position = random.choice(possible_choices)
-            place_symbol(game_board, Computer_symbol, position)
-            if win_check(game_board, Computer_symbol):
-                print(display_board(game_board)
-            game_winner.append(['Computer', Computer_symbol])
-            print(f'Computer wins game{i}')
-            begin_game = True
+            print("Place is already occupied,pls choose another position")
+            player_choice(board)
+    elif full_board_check(board)==True:
+        return 0
+    return user_position
 
-Enquiry=0
-Enquiry=int(input('Enter the game number to get the information: ')
-print('Winner of the round is ',+ game_winner[Enquiry-1])
+import random
+def computer_choice(board):
+    computer_position = random.choice(list(range(1,10)))
+    if board[computer_position] == ' ':
+        board[computer_position] = Computer_choice
+        print(f"computer selection:{computer_position}")
+    elif full_board_check(board)==True:
+        return 0
+    else:
+        computer_choice(board)
+    return computer_position
 
+def winning_criteria(board,symbol):
+    win_data=[[1,2,3],[4,5,6],[7,8,9],[7,4,1],[8,5,2],[9,6,3],[7,5,3],[9,5,1]]
+    for x in range(len(win_data)):
+        if board[win_data[x][0]]==symbol and board[win_data[x][1]]==symbol or board[win_data[x][2]]==symbol:
+            return True
+    return False
+print(display_board(game_board))
+while i <=10:
+    game_board={7:' ',8:' ',9:' ',4:' ',5:' ',6:' ',1:' ',2:' ',3:' '}
+    print(f'Game{i}..' )
+    j = 1
+    while not full_board_check(game_board):
+        print(f"Round{j}")
+        User_turn = player_choice(game_board)
+        Computer_turn = computer_choice(game_board)
+        print(display_board(game_board))
+        if winning_criteria(game_board, User_choice) == True:
+            Winner[i] = 'User'
+        elif winning_criteria(game_board, Computer_choice):
+            Winner[i] = 'Computer'
+        symbol_chosen[j] = [User_choice, Computer_choice]
+        j = j + 1
+    storing.append(Winner[i])
+    board_history.append(game_board)
+    i = i + 1
+
+c = 'y'
+while(c == 'y'):
+    try :
+      ktg= int(input("Enter the game number you want to know about:"))
+      print(storing[ktg - 1])
+      print(f"The gameboard corresponding to the game {ktg} is:\n ")
+      print(display_board(board_history[ktg - 1]))
+    except :
+        print("The game doesn't exist")
+    c = input("Do you wish to continue the game y/n: ")
 
 
 
