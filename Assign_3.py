1cbfe89f3a251710f968a1eb89df09c6dfad3d9c
sreelakshmi587 <90371930+sreelakshmi587@users.noreply.github.com>
2021-09-24 21:39:50 +0530

    Update Assign_3.py

--- Assign_3.py
+++ Assign_3.py
@@ -55,46 +55,43 @@ print("Do you wish to see any of the existing pets or add a new one?")
 
 game_on=True
 while game_on:
-    option = input("Type (y) if you want to add one or else type (n)..(y/n) :")
-    if option[0].lower() == 'y':
-        new_pet = input('Enter the name of the new pet: ')
-        avl_name.append(new_pet)
-        choice = avl_name.index(new_pet)+1
-    elif option[0].lower() =='n':
-        print(avl_name)
-        choice = int(input('Choose from 1-4 to interact with any of the pets shown: '))
-        game_on = False
-    else:
-        print("Sorry,I dont understand..Please enter y or n")
-        continue
-    break
-
-petname=avl_name[choice-1]
-p1 = Pet(petname)
-new_word = input('Enter a new word to teach them :')
-p1.sounds[:].append(new_word)
-interact= int(input('How long do ypu wanna interact with(1-15 preferable): '))
-for i in range(interact):
-    p1.clock_tick()
-    print(p1)
-    if p1.mood()=='hungry':
-        print(f"{petname} is hungry..Feed him to quench the hunger")
-        hungr_opt= input("Are you gonna feed it?(y/n)")
-        if hungr_opt=='y':
+    option = int(input("Type (1) if you want to add one and type (2) to interact with the existing pets: "))
+    if(option==1):
+        print(f'{avl_name} are the list of existing pets')
+        petname=input('Enter the name of the new pet: ')
+        avl_name.append(petname)
+        print(f"{avl_name} is the new list of pets")
+        p1=Pet(petname)
+    elif(option==2):
+        print('You can 1.greet,2.teach or 3.feed the pet')
+        interact=int(input('Choose 1,2 or 3: '))
+        p=int(input('Enter the number of the pet you wanna interact with'))
+        if interact==1:
+            petname=avl_name[p-1]
+            p1=Pet(petname)
+            p1.hi()
+        elif(interact==2):
+            petname=avl_name[p-1]
+            p1 = Pet(petname)
+            word=input('Teach a new word: ')
+            p1.sounds[:].append(word)
+            p1.teach(word)
+        elif(interact==3):
+            petname=avl_name[p-1]
+            p1=Pet(petname)
             p1.feed()
         else:
-            print(f"{petname} is hungry...feed him")
-            continue
-    elif p1.mood()=='bored':
-        print(f"{petname} is bored..Teach him or say hi to reduce its boredom")
-        brd_opt=input('What do you opt--to teach(t) or greet(g) :')
-        if brd_opt[0].lower() =='t':
-            rlx_word=input('Enter a new word to teach : ')
-            p1.sounds[:].append(rlx_word)
-            p1.teach(rlx_word)
-        elif brd_opt[0]=='g':
-            p1.hi()
-        else:
-            print("The pet is bored")
+            print('Invalid choice')
     else:
-        continue
+        print('Invalid choice')
+
+    for i in avl_name:
+        name=Pet(i)
+        name.clock_tick()
+        print(name)
+    game=input('Enter y to continue the game else type n: ')
+    if (game=='y'):
+        game_on = True
+    else:
+        game_on = False
+    print('Hope you have enjoyed!')
